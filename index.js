a9de64a6dd28b72b7e5cc5bb04993ca0fae1d06d
Adnan <azizulislamadnan@gmail.com>
2025-02-03 12:17:04 +0600

    fixed: cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://firebase.web.app'],
+  origin: ['http://localhost:5173', 'https://islampur-jame-masjid.vercel.app'],
 }
 app.use(cors(corsOptions))
 app.use(express.json());
