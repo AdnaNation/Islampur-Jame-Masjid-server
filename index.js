9086710f3321bd6940155672c6be04d61aa3ccde
Adnan <azizulislamadnan@gmail.com>
2025-02-25 19:15:03 +0600

    add individual admin data

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function run() {
     app.get("/admin/:number", async (req, res) => {
       const number = req.params.number
       const query = {number: number}
-      const result = await adminCollection.findOne(number);
+      const result = await adminCollection.findOne(query);
       res.send(result);
     });
     app.get("/users", async (req, res) => {
