b23ddd75ba004edabc9ee8bfc2101fa71185d897
Adnan <azizulislamadnan@gmail.com>
2025-02-26 21:17:34 +0600

    fixing

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function run() {
 
     app.get("/admin/:number", async (req, res) => {
       const number = req.params.number
-      const query = {number: number}
+      const query = {number: number};
       const result = await adminCollection.findOne(query);
       res.send(result);
     });
